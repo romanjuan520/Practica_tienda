3527d677934689542eeb921490f15de444c2c51c
Juan Román <romanjuan520@gmail.com>
2026-03-29 23:40:23 +0200

    se modifico y se pulio las imperfeciones

--- backend/app/services/product_service.py
+++ backend/app/services/product_service.py
@@ -1,43 +1,49 @@
-from ..models.product import Product
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from app.models.product import Product
+from app.schemas.product_schema import ProductCreate
 
-def get_products(db):
-    return db.query(Product).all()
+def get_products(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(Product).offset(skip).limit(limit).all()
 
-def create_product(db, name, price, image, category_id, description, stock):
+def get_product_by_id(db: Session, product_id: int):
+    product = db.query(Product).get(product_id)
 
-    product = Product(
-        name=name,
-        price=price,
-        image=image,
-        category_id=category_id,
-        description=description,
-        stock=stock
-    )
-    db.add(product)
-    db.commit()
-    db.refresh(product)
+    if not product:
+        raise HTTPException(status_code=404, detail="producto no encontrado")
     return product
 
-def update_product(db, product_id, product_data):
+def create_product(db: Session, product: ProductCreate):
+    if product.stock < 0:
+        raise HTTPException(status_code=400, detail="Stock no puede ser negativo")
+    
+    new_product = Product(**product.dict())
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
 
-    product = db.query(Product).filter(Product.id == product_id).first()
+def update_product(db: Session, product_id: int, data: ProductCreate):
+    product = db.query(Product).get(product_id)
 
     if not product:
         raise HTTPException(status_code=404, detail="Producto no encontrado")
     
-    for key, value in product_data.item():
+    for key, value in data.dict(exclude_unset=True).items():
         setattr(product, key, value)
 
-    db.commit()
-    db.refresh(product)
-    return product
+        db.commit()
+        db.refresh(product)
 
+        return product
+    
+def delete_product(db: Session, product_id: int):
+    product = db.query(Product).get(product_id)
 
-def delete_product(db, product_id):
-    product = db.query(Product).filter(Product.id == product_id).first()
     if not product:
         raise HTTPException(status_code=404, detail="Producto no encontrado")
+    
     db.delete(product)
     db.commit()
-    return {"detail": "Producto eliminado"}
+    
+    return {"message": "Producto eliminado correctamente"}
